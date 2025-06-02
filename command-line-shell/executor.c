00e28ec039f68ae07219eb4b8b99e970b3bcf6ea
codeson2003 <carsonsteptech@gmail.com>
2025-06-02 14:09:41 -0400

    cd support working

--- command-line-shell/executor.c
+++ command-line-shell/executor.c
@@ -1,15 +1,32 @@
 #include <stdlib.h>
 #include <unistd.h> // fork, execvp
 #include <stdio.h>
+#include <string.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <sys/stat.h>
 #include "executor.h"
 
 void runCommand(char **args)
 {
+    struct stat st;
     if (args[0] == NULL)
     {
         return; // empty command, do nothing
+    }
+    else if (strcmp(args[0], "cd") == 0)
+    {
+
+        if (stat(args[1], &st) == 0 && S_ISDIR(st.st_mode))
+        {
+            chdir(args[1]);
+            return;
+        }
+        else
+        {
+            perror("cd failed");
+            return;
+        }
     }
 
     pid_t pid = fork();
